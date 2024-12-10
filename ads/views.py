7e655c253ec0f1f9531726dfec7e7e8f93f406dd
Rimas Kalvelis <rkalvelis@gmail.com>
2024-12-10 17:04:23 +0000

    first iteration of owned ads CRUD

--- ads/views.py
+++ ads/views.py
@@ -17,17 +17,15 @@ class AdDetailView(OwnerDetailView):
     model = Ad
 
 
-# class AdCreateView(OwnerCreateView):
-#     model = Ad
-#     # List the fields to copy from the Ad model to the Ad form
-#     fields = ['title', 'text']
-
-# class AdUpdateView(OwnerUpdateView):
-#     model = Ad
-#     fields = ['title', 'text']
-#     # This would make more sense
-#     # fields_exclude = ['owner', 'created_at', 'updated_at']
+class AdCreateView(OwnerCreateView):
+    model = Ad
+    fields_exclude = ["owner", "created_at", "updated_at"]
 
 
-# class AdDeleteView(OwnerDeleteView):
-#     model = Ad
+class AdUpdateView(OwnerUpdateView):
+    model = Ad
+    fields_exclude = ["owner", "created_at", "updated_at"]
+
+
+class AdDeleteView(OwnerDeleteView):
+    model = Ad
