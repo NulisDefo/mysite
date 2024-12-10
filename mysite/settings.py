dd35332c0668582ab4c3470fbee6f33fc25cfe21
Rimas Kalvelis <rkalvelis@gmail.com>
2024-12-10 15:06:05 +0000

    all pyc files removed

--- mysite/settings.py
+++ mysite/settings.py
@@ -4,6 +4,8 @@ Django settings for samples project.
 
 import os
 
+from django.conf.global_settings import DATABASES
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -88,10 +90,23 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "RimasK$ads",
+        "USER": "RimasK",
+        "PASSWORD": "ComplexPassword123",
+        "HOST": "RimasK.mysql.pythonanywhere-services.com",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
