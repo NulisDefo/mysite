1607385dfcfbda50cd32a120c82049570b4bab40
Rimas Kalvelis <rkalvelis@gmail.com>
2025-01-16 11:28:34 +0000

    settings update

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,7 +18,10 @@ SECRET_KEY = "g$iqqu&*mw4_sg3(#ld0sqaalxebel&168^yj%i&sgrw(fmn@w"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+# Configure the domain name using the environment variable
+# that Azure automatically creates for us.
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 # Application definition
 
@@ -99,8 +102,8 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 #         'USER': os.environ.get('DBUSER'),
 #         'PASSWORD': os.environ.get('DBPASS'),
 #         # 'ENGINE': 'django.db.backends.postgresql',
-#         # 'NAME': 'mysiteSample', 
-#         # 'HOST': '127.0.0.1', 
+#         # 'NAME': 'mysiteSample',
+#         # 'HOST': '127.0.0.1',
 #         # 'USER': 'postgres',
 #         # 'PASSWORD': 'ComplexPassword123',
 #     }
