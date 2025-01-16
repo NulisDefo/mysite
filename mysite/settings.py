59368b174d417643f74db2722fb7c178de3cb102
Rimas Kalvelis <rkalvelis@gmail.com>
2025-01-16 09:10:47 +0000

    db update

--- mysite/settings.py
+++ mysite/settings.py
@@ -92,11 +92,22 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mysiteSample', 
+        'USER': 'postgres',
+        'PASSWORD': 'ComplexPassword123',
+        'HOST': '127.0.0.1', 
+        # 'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
 
 # DATABASES = {
 #     "default": {
